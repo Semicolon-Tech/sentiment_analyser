72bf8ce14c87faeeb253593a927b51a973f4841c
Joshua Christian <joshua3christianx@gmail.com>
2020-11-24 10:53:35 +0100

    cleaned up the react application

--- src/contexts/ModelParametersContext.jsx
+++ src/contexts/ModelParametersContext.jsx
@@ -5,8 +5,6 @@ export const ModelParametersContext = createContext();
 export const ModelParametersProvider = props => {
     const [modelParameters, setModelParameters] = useState({
         "text": "do no evil",
-        "model": "categorical",
-        "vectorizer": "count"
     })
 
     return (
